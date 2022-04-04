412651f697453f94bfcf0350cac2d925faaee47d
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-04-04 11:56:00 +0100

    Changed JDK version

--- src/main/java/com/emirex/cicdgithubactions/web/AppController.java
+++ src/main/java/com/emirex/cicdgithubactions/web/AppController.java
@@ -17,7 +17,7 @@ public class AppController {
 
 
 
-        git commit -m "first commit"
-        git branch -M main
-        git remote add origin https://github.com/ChukwuEmeka1269/CICD-demo.git
-        git push -u origin main
+
+
+
+
